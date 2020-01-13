f1970edb6c6cfc8a4e56ebb3a490922379c06d8a
ea777 <116373066@umail.ucc.ie>
2020-01-13 21:48:26 +0000

    downloaind excel files

--- analysis/views.py
+++ analysis/views.py
@@ -6,17 +6,33 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 import numpy as np
 import os
+from analysis import aws
+import json
+import boto3
+
+
+import re
 
 
 # Create your views here.
 def index(request):
+
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    my_excelfile = os.path.join(THIS_FOLDER, 'bunsen_reviews.xls')
+    if len(os.listdir(os.path.join(THIS_FOLDER, "excelfiles"))) == 0:
+        print("Directory is empty")
+        aws.pulling_excelfiles()
+    else:
+        print("Directory is not empty")
+
+    my_excelfile = os.path.join(THIS_FOLDER, 'excelfiles/excelfilesbunsen-burger_reviews.xlsx')
 
     # read form excel as dataframe
     df = pd.read_excel(my_excelfile, sheet_name='Sheet1')
     # no need to display sentimental analysis and no.of reviews, so drop it
-    df = df.iloc[:, :-2]
+    # df = df.iloc[:, :-2]
+
+    del df['No. of reviews']
+
 
     print("Column headings:")
     print(df.columns)
@@ -25,10 +41,23 @@ def index(request):
     df['Name'].replace('', np.nan, inplace=True)
     df['Review'].replace('', np.nan, inplace=True)
     df['Date'].replace('', np.nan, inplace=True)
+    df['SA'].replace('', np.nan, inplace=True)
+
+
 
     df.dropna(subset=['Name'], inplace=True)
     df.dropna(subset=['Review'], inplace=True)
     df.dropna(subset=['Date'], inplace=True)
+    df.dropna(subset=['SA'], inplace=True)
+
+    for ind in df.index:
+        # print(df['SA'][ind])
+        df['SA'][ind].replace('\n', "")
+        sentiment_object = json.loads(df['SA'][ind])
+        # sentiment_column = sentiment_object.sentiment.document.score
+        # print(sentiment_column)
+        print(sentiment_object.sentiment.document.score)
+
 
     # Create an empty list
     Row_list = []
@@ -36,21 +65,16 @@ def index(request):
     # Iterate over each row
     for rows in df.itertuples():
         # Create list for the current row
-        my_list = [rows.Name, rows.Review, rows.Date]
+        my_list = [rows.Name, rows.Review, rows.Date, rows.SA]
 
         # append the list to the final list
         Row_list.append(my_list)
 
     # Print the list
-    print(Row_list)
+    # print(Row_list)
 
     # pass the list to template
 
     return render(request, 'index.html', {"response": Row_list})
 
-class PostListView():
 
-  template_name = 'index.html'
-  context_object_name = 'rows'
-  ordering = ['-date_posted']
-  paginate_by = 5
