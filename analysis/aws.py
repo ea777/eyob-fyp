7814aa842c349596ed5cce362b72c11b299d5776
ea777 <116373066@umail.ucc.ie>
2020-01-14 23:03:56 +0000

    download all files

--- analysis/aws.py
+++ analysis/aws.py
@@ -1,24 +1,42 @@
-#Reference: https://realpython.com/python-boto3-aws-s3
-
-
+# #Reference: https://realpython.com/python-boto3-aws-s3
 def pulling_excelfiles():
+
+    from shutil import move
     import boto3
-
-    s3_resource = boto3.resource('s3')
-
-    s3_resource.Object('fypsentanalysis', 'bunsen-burger_reviews.xlsx').download_file(
-    f'C:/Users/eyob/PycharmProjects/reviewanalysis/analysis/excelfiles/excelfiles{"bunsen-burger_reviews.xlsx"}')
-
-# s3_resource.Object('fypsentanalysis', 'IS4419.docx').upload_file(
-#     Filename=f'C:/Users/eyob/OneDrive/Desktop/IS4419.docx')
+    import os
 
 
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+    s3=boto3.client('s3')
+    list=s3.list_objects(Bucket='fypsentanalysis')['Contents']
+    print(list)
+    for key in list:
+        s3.download_file('fypsentanalysis', key['Key'], key['Key'])
+        source = os.path.join(THIS_FOLDER, key['Key'])
+        destination_folder = os.path.join(THIS_FOLDER, "excelfiles")
+        destination = os.path.join(destination_folder, key['Key'])
+        move(source, destination)
 
 
-# for bucket in s3.buckets.all():
-#     print(bucket.name)
-#
-#     my_bucket = s3.Bucket('fypsentanalysis')
-#
-#     for file in my_bucket.objects.all():
-#         print(file.key)
+def upload_file(file_name, object_name=None):
+    import logging
+    import boto3
+    from botocore.exceptions import ClientError
+    """Upload a file to an S3 bucket
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, 'fypsentanalysis')
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
